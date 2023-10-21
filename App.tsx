f2af41fc0cf261c98432250d3220be0c6340fafd
tahmidmahdi <tahmid5550@gmail.com>
2023-10-21 23:06:23 +0600

    Navigator added

--- App.tsx
+++ App.tsx
@@ -4,88 +4,58 @@
  *
  * @format
  */
-
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import type { PropsWithChildren } from 'react';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import { Colors, Header, LearnMoreLinks } from 'react-native/Libraries/NewAppScreen';
-import Text from './src/components/text/text';
+import { StyleSheet, useColorScheme } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Home from './src/screens/Home/Home';
+import colors from './src/themes/colors';
+const Stack = createStackNavigator();
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
-const Stack = createStackNavigator();
-
-function Section({ children, title }: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  console.log('🚀 ~ file: App.tsx:30 ~ isDarkMode:', isDarkMode);
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.darker : colors.primaryDark,
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle="light-content" backgroundColor={backgroundStyle.backgroundColor} />
-      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}
-        >
-          {/* <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come
-            back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">Read the docs to discover what to do next:</Section> */}
-          <Text preset="small">Hello</Text>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView style={backgroundStyle}>
+    //   <StatusBar barStyle="light-content" backgroundColor={backgroundStyle.backgroundColor} />
+    //   <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+    //     <Header />
+    //     <View
+    //       style={{
+    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    //       }}
+    //     >
+    //       {/* <Section title="Step One">
+    //         Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come
+    //         back to see your edits.
+    //       </Section>
+    //       <Section title="See Your Changes">
+    //         <ReloadInstructions />
+    //       </Section>
+    //       <Section title="Debug">
+    //         <DebugInstructions />
+    //       </Section>
+    //       <Section title="Learn More">Read the docs to discover what to do next:</Section> */}
+    //       <Text preset="small">Hello</Text>
+    //       <LearnMoreLinks />
+    //     </View>
+    //   </ScrollView>
+    // </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
